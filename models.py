5bebbf577772e18bd3e7c2ae00705e4e69626001
Prrrf <prrrffrrrp@gmail.com>
2017-12-14 07:08:36 +0100

    Configure database and create models

---
+++ models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from app import db
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(64), unique=True, nullable=False)
+    subtitle = db.Column(db.Text(), nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    timestamp = db.Column(db.Date(), default=datetime.uctnow)
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
