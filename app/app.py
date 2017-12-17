38430ad570e59982af93adb0ef960d74add432e2
Prrrf <prrrffrrrp@gmail.com>
2017-12-17 08:41:31 +0100

    Create first template

--- app/app.py
+++ app/app.py
@@ -1,19 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 from .models import Post
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 @app.route('/<name>')
