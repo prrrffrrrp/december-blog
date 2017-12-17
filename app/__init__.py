a0552d9b444cf3ef3eaf2d4346eed6196854fd87
Prrrf <prrrffrrrp@gmail.com>
2017-12-17 13:17:16 +0100

    Put secret_key in env variable

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'to-be-changed'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
