2104e7e509e6c3a87235a1062bbc6cff7d1cac05
Prrrf <prrrffrrrp@gmail.com>
2017-12-18 09:13:44 +0100

    Test. Minor change in env variable acquisition

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app import create_app, db
 from app.models import Post
 from flask_migrate import Migrate
 
-app = create_app(os.getenv('APP_SETTINGS'))
+app = create_app(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 
