64da51046ef99efd1f612d249a9285df4156be43
Prrrf <prrrffrrrp@gmail.com>
2017-12-22 06:39:45 +0100

    Create user registration procedure

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+# local imports:
+from ..models import User
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(),
+                                         EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email is already in use')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username is already in use')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,24 @@
 from flask import render_template, url_for, redirect, flash
 
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
+from ..models import User
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data,)
+        db.session.add(user)
+        db.session.commit()
+        flash('You have registered and may login')
+        return redirect(url_for('auth.login'))
+
+    return render_template('register.html', form=form)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
