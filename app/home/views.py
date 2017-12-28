e0f859e9fea6ff0ce1dd7ce79df661ca14ad7aba
Prrrf <prrrffrrrp@gmail.com>
2017-12-28 08:13:32 +0100

    Fix bug: Unpublished posts accessible from url.
    Fix: Abort with 403 error if post is unpublished or user is not admin.

--- app/home/views.py
+++ app/home/views.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, redirect, session, flash
+from flask import abort, render_template
+from flask_login import current_user
 
 from . import home
 from ..models import Post
@@ -6,11 +7,19 @@ from ..models import Post
 
 @home.route('/')
 def index():
-    posts = Post.query.order_by(Post.timestamp.desc()).filter_by(publish=True).all()
+    posts = Post.query.order_by(
+        Post.timestamp.desc()).filter_by(publish=True).all()
     return render_template('index.html', posts=posts)
 
 
 @home.route('/posts/<int:id>')
 def post(id):
     post = Post.query.get_or_404(id)
-    return render_template('post.html', post=post)
+
+    try:
+        if post.publish or current_user.is_admin:
+            return render_template('post.html', post=post)
+        else:
+            abort(403)
+    except AttributeError:
+        abort(403)
